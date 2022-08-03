30fc6f8edb6191e76b9ccbeba14c10892c102434
carbonscott <wangimagine@gmail.com>
2022-08-03 15:17:08 -0700

    Stucture the repo to make it run.

--- peaknet/train.py
+++ peaknet/train.py
@@ -36,7 +36,7 @@ def train(model, device, params, writer):
         print(str(key) + ' : ' + str(value))
 
     print('')
-    print("Will show intermediate activation: ") + str(hasattr(model, 'can_show_inter_act') and model.can_show_inter_act) + "."
+    print(f"Will show intermediate activation: {getattr(model, 'can_show_inter_act', 'Not available')}.")
 
     if params["n_classes"] == 1:
         loss_func = PeakNetBCE1ChannelLoss(params, device).to(device)
